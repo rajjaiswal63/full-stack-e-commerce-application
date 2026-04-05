c5fb1ee8f1c7184110511d2f168b21bac880773e
Raj jaiswal <rajjaiswal6394@gmail.com>
2026-04-05 18:58:27 +0530

    project started

---
+++ src/main/java/org/ecommerce/project/controller/CategoryController.java
@@ -0,0 +1,22 @@
+package org.ecommerce.project.controller;
+
+import org.ecommerce.project.model.Category;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CategoryController {
+    private List<Category> categories = new ArrayList<>();
+
+    @GetMapping("/api/public/categories")
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
+}
