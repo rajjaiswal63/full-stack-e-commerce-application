a0a3bb9602eb6aec5c416eec0f5a8df49624b71a
Raj jaiswal <rajjaiswal6394@gmail.com>
2026-04-11 08:22:59 +0530

    categories phase one done

--- src/main/java/org/ecommerce/project/controller/CategoryController.java
+++ src/main/java/org/ecommerce/project/controller/CategoryController.java
@@ -1,22 +1,35 @@
 package org.ecommerce.project.controller;
 
 import org.ecommerce.project.model.Category;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.ecommerce.project.service.CategoryService;
 
-import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
 import java.util.List;
 
 @RestController
 public class CategoryController {
-    private List<Category> categories = new ArrayList<>();
+    @Autowired
+    private CategoryService categoryService;
 
     @GetMapping("/api/public/categories")
     public List<Category> getCategories() {
-        return categories;
+       return categoryService.getAllCategories();
     }
 
-    public void setCategories(List<Category> categories) {
-        this.categories = categories;
+    @PostMapping("/api/public/categories")
+    public String createCategory(@RequestBody Category category) {
+         categoryService.createCategory(category);
+        return "Category added successfully";
     }
+
+    @DeleteMapping("/api/admin/categories/{categoryId}")
+    public String deleteCategory(@PathVariable Long categoryId){
+        String status = categoryService.deleteCategory(categoryId);
+        return status;
+    }
+
+
 }
--- src/main/java/org/ecommerce/project/model/Category.java
+++ src/main/java/org/ecommerce/project/model/Category.java
@@ -1,6 +1,8 @@
 package org.ecommerce.project.model;
 
+
 public class Category {
+
     private Long categoryId;
     private String categoryName;
 
