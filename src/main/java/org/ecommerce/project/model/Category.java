2ec55bb0f26b997b536d2fe925182cf50cf443be
Raj jaiswal <rajjaiswal6394@gmail.com>
2026-04-14 08:38:51 +0530

    db connected

--- src/main/java/org/ecommerce/project/model/Category.java
+++ src/main/java/org/ecommerce/project/model/Category.java
@@ -1,14 +1,24 @@
 package org.ecommerce.project.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity(name = "CATEGORY")
 public class Category {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
     private String categoryName;
 
     public Category(Long categoryId, String categoryName) {
         this.categoryId = categoryId;
         this.categoryName = categoryName;
+    }
+
+    public Category() {
+
     }
 
     public Long getCategoryId() {
